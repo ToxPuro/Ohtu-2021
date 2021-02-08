b7d2498e7efe966e94b3825fe81dd586c135abf5
ToxPuro <puro.touko2@gmail.com>
2021-02-08 15:44:59 +0200

    Tehtävä 1

---
+++ viikko3/nhl-reader/src/index.py
@@ -0,0 +1,26 @@
+import requests
+from player import Player
+
+def main():
+    url = "https://nhlstatisticsforohtu.herokuapp.com/players"
+    response = requests.get(url).json()
+
+
+    players = []
+
+    for player_dict in response:
+        player = Player(
+            player_dict['name'], player_dict['team'],
+            player_dict['goals'], player_dict['assists']
+        )
+
+        players.append(player)
+
+    print("Players from FIN 2021-01-04 19:15:32.85661")
+    print("")
+
+    for player in players:
+        print(player)
+
+if __name__=='__main__':
+    main()
